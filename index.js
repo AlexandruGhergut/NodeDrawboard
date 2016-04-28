13f2d6392ed4a1c9b3a95c4fa719abe480c92437
Alexandru Ghergut <alexandru.ghergut94@gmail.com>
2016-04-28 18:51:19 +0300

    Implement client-side chat

--- index.js
+++ index.js
@@ -3,9 +3,9 @@ var app = express();
 var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var path = require("path");
+
 var PORT = process.argv.length <= 2 ? 3000 : process.argv[2];
 
-console.log(process.argv.length);
 app.use("/assets/", express.static(__dirname + "/assets"));
 
 app.get('/', function(request, response){
